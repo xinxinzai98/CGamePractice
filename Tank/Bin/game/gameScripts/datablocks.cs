99771fc1dc2745f8e969a7512a52c0bb7a9ff8a0
xinxinzai98 <894643096@qq.com>
2017-07-05 19:54:52 +0800

    program start2

--- Tank/Bin/game/gameScripts/datablocks.cs
+++
@@ -1,8 +0,0 @@
-//---------------------------------------------------------------------------------------------
-// Torque Game Builder
-// Copyright (C) GarageGames.com, Inc.
-//---------------------------------------------------------------------------------------------
-//
-// This is the file you should define your custom datablocks that are to be used
-// in the editor.
-//
--- Tank/Bin/game/managed/brushes.cs
+++
@@ -1,5 +0,0 @@
-$brushSet = new SimSet() {
-   canSaveDynamicFields = "1";
-   internalName = "Brushes";
-      setType = "Brushes";
-};
